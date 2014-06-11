80c54b22151d6fbbb4419e8b0cb6950f22cc9d14
Arash Rouhani <miffoljud@gmail.com>
2014-06-11 22:35:52 +0200

    Update arash.zsh

--- arash.zsh
+++ arash.zsh
@@ -12,9 +12,11 @@
 
   local EXTRA_HC_OPTS
   local EXTRA_RUNTEST_OPTS
-  EXTRA_HC_OPTS=( -g )
-  EXTRA_RUNTEST_OPTS=( +RTS --stack-trace -RTS )
-  local LOG_FILE_NAME="log-fast-1${EXTRA_HC_OPTS}${EXTRA_RUNTEST_OPTS[2]}"
+  # EXTRA_HC_OPTS=( -g )
+  # EXTRA_RUNTEST_OPTS=( +RTS --stack-trace -RTS )
+  EXTRA_HC_OPTS=()
+  EXTRA_RUNTEST_OPTS=()
+  local LOG_FILE_NAME="log-fast-1${EXTRA_HC_OPTS}${EXTRA_RUNTEST_OPTS[2]}-$DATE"
   # ${EXTRA_RUNTEST_OPTS[2]} hack evaluates to --stack-trace
 
   make clean                                         \
@@ -24,4 +26,8 @@
         EXTRA_RUNTEST_OPTS="$EXTRA_RUNTEST_OPTS" \
         mode=fast NoFibRuns=1                        \
     | tee "results/${LOG_FILE_NAME}"             \
+}
+
+analyse () {
+ ./nofib-analyse/nofib-analyse log-fast-1-wtih-dash-g log-fast-1-stack-trace | less
 }
